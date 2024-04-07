d453ed0b2394d8acc8a05a8f19934e377609f56b
Ajithjaya <ajith30022@gmail.com>
2024-04-08 00:13:44 +0530

    basic test cases

---
+++ src/test/java/digit/enrichment/BirthApplicationEnrichmentTest.java
@@ -0,0 +1,88 @@
+package digit.enrichment;
+
+import digit.service.UserService;
+import digit.util.IdgenUtil;
+import digit.web.models.*;
+import org.egov.common.contract.request.RequestInfo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+public class BirthApplicationEnrichmentTest {
+
+    @InjectMocks
+    private BirthApplicationEnrichment birthApplicationEnrichment;
+
+    @Mock
+    private IdgenUtil idgenUtil;
+
+    @Mock
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void enrichBirthApplication() {
+        BirthRegistrationRequest birthRegistrationRequest = createBirthRegistrationRequest();
+        when(idgenUtil.getIdList(any(), any(), any(), any(), anyInt()))
+                .thenReturn(Collections.singletonList("BTR123456"));
+
+        birthApplicationEnrichment.enrichBirthApplication(birthRegistrationRequest);
+
+        List<BirthRegistrationApplication> applications = birthRegistrationRequest.getBirthRegistrationApplications();
+        assertNotNull(applications);
+        assertEquals(2, applications.size());
+        for (BirthRegistrationApplication application : applications) {
+            assertNotNull(application.getAuditDetails());
+            assertNotNull(application.getId());
+            assertNotNull(application.getApplicationNumber());
+            assertNotNull(application.getAddress());
+            assertNotNull(application.getFather());
+            assertNotNull(application.getMother());
+            assertNotNull(application.getAddress().getId());
+            assertEquals(application.getId(), application.getFather().getUuid());
+            assertEquals(application.getId(), application.getMother().getUuid());
+        }
+    }
+
+    @Test
+    void enrichBirthApplicationUponUpdate() {
+        BirthRegistrationRequest birthRegistrationRequest = createBirthRegistrationRequest();
+        BirthRegistrationApplication application = birthRegistrationRequest.getBirthRegistrationApplications().get(0);
+        AuditDetails auditDetails = new AuditDetails("userId1", "123456L", Long.parseLong("userId2"), 123457L);
+        application.setAuditDetails(auditDetails);
+
+        birthApplicationEnrichment.enrichBirthApplicationUponUpdate(birthRegistrationRequest);
+
+        assertEquals("userId1", application.getAuditDetails().getCreatedBy());
+        assertEquals(123456L, application.getAuditDetails().getCreatedTime());
+        assertEquals("userId2", application.getAuditDetails().getLastModifiedBy());
+        assertNotEquals(123457L, application.getAuditDetails().getLastModifiedTime()); // Last modified time should be updated
+    }
+
+    private BirthRegistrationRequest createBirthRegistrationRequest() {
+        RequestInfo requestInfo = new RequestInfo();
+        List<BirthRegistrationApplication> applications = new ArrayList<>();
+        BirthRegistrationApplication application = new BirthRegistrationApplication();
+        application.setTenantId("tenantId");
+        application.setAddress(new BirthApplicationAddress());
+        application.setFather(new User());
+        application.setMother(new User());
+        applications.add(application);
+        return new BirthRegistrationRequest(requestInfo, applications);
+    }
+}
--- src/test/java/digit/web/controllers/BirthApiControllerTest.java
+++ src/test/java/digit/web/controllers/BirthApiControllerTest.java
@@ -1,28 +1,17 @@
 package digit.web.controllers;
 
-import digit.web.models.BirthApplicationSearchCriteria;
-import digit.web.models.BirthRegistrationRequest;
-import digit.web.models.BirthRegistrationResponse;
-import digit.web.models.ErrorResponse;
-import org.junit.Test;
+
 import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import digit.TestConfiguration;
 
-    import java.util.ArrayList;
-    import java.util.HashMap;
-    import java.util.List;
-    import java.util.Map;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
